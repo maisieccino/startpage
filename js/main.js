9f141ac3e8fdf4456faa995c4e2e42ff89e0be5f
Matthew Bell <logicnotlogical@outlook.com>
2015-05-26 00:14:41 +0100

    KEYBINDS

--- js/main.js
+++ js/main.js
@@ -2,3 +2,26 @@ $('.section').click(function () {
 	$(this).find("ul").toggleClass("hideLinks");
 	$('.section').not(this).find("ul").addClass("hideLinks");
 });
+
+$(document).keyup(function (event) {
+	if (!$(event.target).is("input:focus")) {
+		if (event.which >= 65 && event.which <= 90) {
+			$('.section[data-letter="'+String.fromCharCode(event.which)+'"]').click();
+		}
+		else if (event.which == 49) {
+			$('.searchbox').focus();
+		}
+	}
+	else {
+		if (event.which==27) {
+			$('.searchbox').blur();
+		}
+	}
+});
+
+$(document).load(function() {
+	if (getHours()<12) {
+		$('.dateTime').html('Good morning! ');
+	}
+	else $('.dateTime').html('Good afternoon! ');
+});
