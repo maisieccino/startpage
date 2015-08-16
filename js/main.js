e05341216c35e91b844386588a89b364623549e5
logicnotlogical <logicnotlogical@outlook.com>
2015-08-16 14:35:14 +0100

    1.0: Added MIT license.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,29 @@
 $('.section').click(function () {
 	$(this).find("ul").toggleClass("hideLinks");
 	$('.section').not(this).find("ul").addClass("hideLinks");
+	if($(this).find("ul").hasClass("hideLinks")) {
+		currentPaneOpen = "";
+	}
+	else {
+		currentPaneOpen = $(this).attr("data-letter");
+	}
 });
+
+var currentPaneOpen = "";
 
 $(document).keyup(function (event) {
 	if (!$(event.target).is("input:focus")) {
 		if (event.which >= 65 && event.which <= 90) {
 			$('.section[data-letter="'+String.fromCharCode(event.which)+'"]').click();
 		}
-		else if (event.which == 49) {
+		else if (event.which == 220) {
 			$('.searchbox').focus();
+		}
+		else if (event.which >= 49 && event.which <= 57) {
+			window.location.href = $('.section[data-letter="'+currentPaneOpen+'"] ul li:nth-child('+(event.which-48)+') a').attr("href");
+		}
+		else if (event.which == 48) {
+			window.location.href = $('.section[data-letter="'+currentPaneOpen+'"] ul li:nth-child(10) a').attr("href");
 		}
 	}
 	else {
@@ -23,8 +37,9 @@ $(document).keyup(function (event) {
 	}
 });
 
-$(document).load(function() {
-	if (getHours()<12) {
+$(document).ready(function() {
+	var d = new Date();
+	if (d.getHours()<12) {
 		$('.dateTime').html('Good morning! ');
 	}
 	else $('.dateTime').html('Good afternoon! ');
